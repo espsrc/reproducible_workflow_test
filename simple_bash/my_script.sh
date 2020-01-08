a234a00d17f090d0da9c6c55b7d2b23bab44fb9d
jmoldon <jmoldon@iaa.es>
2020-01-08 14:35:56 +0100

    Add a step to bash to use casacore to read amplitudes

--- simple_bash/my_script.sh
+++ simple_bash/my_script.sh
@@ -1,5 +1,6 @@
 # Step 1
 # Download some data to directory input
+# Dependencies: 
 # Input: 
 # Output: data/interim/TEST1_L.fits
 mkdir data
@@ -10,6 +11,7 @@ mv TEST1_L.fits data/raw
 
 # Step 2
 # Execute casa_script.py
+# Dependencies: casa
 # Input: data/raw/TEST1_L.fits
 # Output: data/interim/TEST1_L.ms
 rm -r data/interim
@@ -18,3 +20,11 @@ rm -r output
 mkdir output
 casa --nologger --nogui -c ./src/casa_script.py
 
+
+# Step 3 
+# Use python-casacore to read amplitudes from MS and produce numpy file
+# Dependencies: python, python-casacore, numpy
+# Input: data/interim/TEST1_L.ms
+# Output: data/interim/amp.npz
+rm -r data/interim/amp.npz
+python ./src/read_amplitudes.py -msfile data/interim/TEST1_L.ms
